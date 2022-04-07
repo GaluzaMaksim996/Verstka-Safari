bbe1cb3dd0197dd90fc98405182f04dc117f5839
Maksim <krakket@bk.ru>
2022-04-07 13:54:04 +0300

    settings for deploy

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ $(function(){
 	});
 
   $('.gallery__slider').slick({
-    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="/images/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="/images/arrow-right.svg" alt=""></button>'
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>'
   });
 
   $('.gallery__item-inner').magnificPopup({
